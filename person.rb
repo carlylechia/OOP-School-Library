9861b9c89604c0f4dad678cdf643e7137f347ca7
Chia Carlyle <chiacarlyle@gmail.com>
2022-07-18 20:01:24 +0100

    Fix linters errors.

--- person.rb
+++ person.rb
@@ -10,16 +10,16 @@ class Person
   end
 
   def can_use_services?
-    return true if is_of_age? || parent_permission == true
+    return true if of_age? || parent_permission == true
 
     false
   end
 
-  def is_of_age?
+  def of_age?
     return true if @age >= 18
 
     false
   end
 
-  private :is_of_age?
+  private :of_age?
 end
