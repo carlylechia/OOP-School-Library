faf0478cd2f88c34265b15860721f66374676e83
Chia Carlyle <chiacarlyle@gmail.com>
2022-07-20 11:10:17 +0100

    Create Book class, set attributes and associations

---
+++ book.rb
@@ -0,0 +1,16 @@
+class Book
+  attr_accessor :title, :author
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def rental_book(rental)
+    return unless rental.instance_of?(Rental) && !@rentals.include?(rental)
+
+    @rentals.push(rental)
+    rental.book_rentaled(self)
+  end
+end
