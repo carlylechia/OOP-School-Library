30a02198ee119ac0ceeee500a2df6819c202e7db
Chia Carlyle <chiacarlyle@gmail.com>
2022-07-25 15:31:39 +0100

    Start refactoring main.rb file

--- main.rb
+++ main.rb
@@ -1,52 +1,47 @@
 #!/usr/bin/env ruby
 
-require './app'
+require_relative './app'
 
-class Main
-  def self.home_page
-    puts "\nPlease enter a number:\n "
+puts "\nWelcome to the OOP School Library App.\n "
 
-    @content = {
-      '1' => 'List all books',
-      '2' => 'List all people',
-      '3' => 'Create a person (teacher or student)',
-      '4' => 'Create a book',
-      '5' => 'Create a rental',
-      '6' => 'List all rentals for a given person id',
-      '7' => 'Exit'
-    }
-    @content.each { |index, string| puts "#{index} - #{string}" }
-
-    Integer(gets.chomp)
-  end
-
-  app = App.new
-
-  loop do
-    case home_page
-    when 1
-      app.booklist
-    when 2
-      app.peoplelist
-    when 3
-      app.create_person
-    when 4
-      app.create_book
-    when 5
-      app.create_rental
-    when 6
-      app.rentalslist
-    when 7
-      puts "\nThanks for using this app. See you soon!\n"
-      exit
-    else
-      puts "\nPlease select a number from the list!"
-    end
-  end
+def options_list
+  puts "Please choose an option by selecting a number:
+              1. List all books
+              2. List all people
+              3. Create person account
+              4. Create a book
+              5. Create a rental
+              6. List all rentals for a given person ID
+              7. Exit"
+  gets.chomp
 end
 
 def main
-  App.new
+  app = App.new
+  home_options
 end
 
-Main
+def home_options
+  case options_list
+  when 1
+    app.booklist
+  when 2
+    app.peoplelist
+  when 3
+    app.create_person
+  when 4
+    app.create_book
+  when 5
+    app.create_rental
+  when 6
+    app.rentalslist
+  when 7
+    puts "\nThanks for using this app. See you next time!\n"
+    exit
+  else
+    puts "\nPlease select a number from the list!"
+  end
+  home_options
+end
+
+main
